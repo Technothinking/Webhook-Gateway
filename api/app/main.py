09dfc8973d82c68e93cb2dcee4d6f5ae7571938d
Nitish Singh <nitishsinghb007@gmail.com>
2026-09-22 21:08:22 +0530

    Updated the include_router with the new route names

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.routes.events import eventRouter
+from app.routes.events import router as event_router
+from app.routes.subscribers import router as subscriber_router
 
 app = FastAPI(title="Webhook Reliability Gateway")
 
@@ -7,4 +8,5 @@ app = FastAPI(title="Webhook Reliability Gateway")
 async def root():
     return {"message": "Webhook Reliability Gateway API is running."}
 
-app.include_router(eventRouter)
+app.include_router(event_router)
+app.include_router(subscriber_router)
